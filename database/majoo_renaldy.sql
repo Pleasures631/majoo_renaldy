93be6d2c422068d4b0a85a3ac1ed63616e6d5a62
Renaldy Cahya <renaldycahyaa@gmail.com>
2022-06-05 20:45:34 +0700

    commit db

---
+++ database/majoo_renaldy.sql
@@ -0,0 +1,89 @@
+/*
+SQLyog Ultimate v11.33 (64 bit)
+MySQL - 5.7.36 : Database - kbtest_renaldy
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `t_category` */
+
+DROP TABLE IF EXISTS `t_category`;
+
+CREATE TABLE `t_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(255) DEFAULT NULL,
+  `created_by` varchar(255) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_category` */
+
+insert  into `t_category`(`category_id`,`category_name`,`created_by`,`created_date`) values (1,'Elektronik',NULL,NULL),(2,'Permainan Game',NULL,NULL);
+
+/*Table structure for table `t_product` */
+
+DROP TABLE IF EXISTS `t_product`;
+
+CREATE TABLE `t_product` (
+  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(50) DEFAULT NULL,
+  `created_by` varchar(50) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_product` */
+
+insert  into `t_product`(`product_id`,`product_name`,`created_by`,`created_date`,`updated_by`,`updated_date`) values (1,'Pc baru',NULL,'2022-06-05 13:04:49',NULL,NULL);
+
+/*Table structure for table `t_product_detail` */
+
+DROP TABLE IF EXISTS `t_product_detail`;
+
+CREATE TABLE `t_product_detail` (
+  `product_detail_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `product_amount` varchar(255) DEFAULT NULL,
+  `product_pict` varchar(255) DEFAULT NULL,
+  `product_desc` varchar(255) DEFAULT NULL,
+  `created_by` varchar(50) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `updated_by` varchar(50) DEFAULT NULL,
+  `updated_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`product_detail_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_product_detail` */
+
+insert  into `t_product_detail`(`product_detail_id`,`product_id`,`category_id`,`product_amount`,`product_pict`,`product_desc`,`created_by`,`created_date`,`updated_by`,`updated_date`) values (1,1,1,'5000000','paket-desktop.png','deskripsi',NULL,'2022-06-05 13:04:49',NULL,NULL);
+
+/*Table structure for table `t_user` */
+
+DROP TABLE IF EXISTS `t_user`;
+
+CREATE TABLE `t_user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_user` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
